7888cb82f797de09d96720b0610ba47dbb567958
leo <leonardo.stefani.ge@gmail.com>
2017-04-29 08:45:23 +0200

    Ultima

--- miniblink/usart_irq.cpp
+++ miniblink/usart_irq.cpp
@@ -2,7 +2,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
-#include <libopencm3/cm3/nvic.h>
+ #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 
 typedef int32_t ring_size_t;
