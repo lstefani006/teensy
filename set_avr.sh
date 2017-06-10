c0e5cef5f3f6b63c72215949b41f84a7f490f5ab
leo <leonardo.stefani.ge@gmail.com>
2017-06-10 19:05:40 +0200

    ididididi

--- set_avr.sh
+++ set_avr.sh
@@ -1,5 +1,16 @@
 #export PATH=$PATH:$HOME/arduino-1.6.11/hardware/tools/avr/bin
-#export ARDUINO=$HOME/arduino-1.6.11
+
+if [ -e $HOME/arduino-1.6.12 ] ; then
+	export ARDUINO=$HOME/arduino-1.6.12
+fi
+
+if [ -e $HOME/gcc-site/avr-gcc-7.1.0 ] ; then
+	export PATH=$PATH:$HOME/gcc-site/avr-gcc-7.1.0/bin
+fi
+if [ -e $HOME/gcc-site/avr-gcc-6.2.0 ] ; then
+	export PATH=$PATH:$HOME/gcc-site/avr-gcc-6.2.0/bin
+fi
+
 
 export PATH=$PATH:$HOME/gcc-site/avr-gcc-7.1.0/bin:/home/leo/teensy/AvrLib
 if [ -e $HOME/arduino-1.6.11 ] ; then
