629889c2db65e38771c555242ba07be16b7a5863
leo <leonardo.stefani.ge@gmail.com>
2017-06-25 07:00:42 +0200

    dididid

--- set_avr.sh
+++ set_avr.sh
@@ -1,24 +1,25 @@
 #export PATH=$PATH:$HOME/arduino-1.6.11/hardware/tools/avr/bin
 
+if [ -e $HOME/arduino-1.8.3 ] ; then
+	export ARDUINO=$HOME/arduino-1.8.3
+fi
 if [ -e $HOME/arduino-1.8.2 ] ; then
 	export ARDUINO=$HOME/arduino-1.8.2
-	echo arduino 1.8.2
 fi
 if [ -e $HOME/arduino-1.6.12 ] ; then
 	export ARDUINO=$HOME/arduino-1.6.12
-	echo arduino 1.6.12
 fi
 if [ -e $HOME/arduino-1.6.11 ] ; then
 	export ARDUINO=$HOME/arduino-1.6.11
-	echo arduino 1.6.11
 fi
 
+echo ARDUINO=$ARDUINO
+
+########################################
 # per lanciare e stoppare ArduinoSerialMonitor.exe
 export PATH=$PATH:$HOME/teensy
 
 ########################################
-
-
 
 if [ -e $HOME/gcc-site/avr-gcc-7.1.0 ] ; then
 	export PATH=$PATH:$HOME/gcc-site/avr-gcc-7.1.0/bin
@@ -26,9 +27,7 @@ fi
 if [ -e $HOME/gcc-site/avr-gcc-6.2.0 ] ; then
 	export PATH=$PATH:$HOME/gcc-site/avr-gcc-6.2.0/bin
 fi
+echo PATH=$PATH
 
-
+########################################
 export PLATFORM=AVR
-
-
-echo $ARDUINO
