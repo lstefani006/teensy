f8351d49c8dc0d213b54595033cc64765c18afd3
leo <leonardo.stefani.ge@gmail.com>
2017-12-28 08:41:43 +0100

    VSCode

--- Blink/Blink.cpp
+++ Blink/Blink.cpp
@@ -5,10 +5,12 @@
 // Pin 11 has the LED on Teensy 2.0
 // Pin 6  has the LED on Teensy++ 2.0
 // Pin 13 has the LED on Teensy 3.0
-#ifdef ARDUINO
-int led = PC13;
-#else
-int led = PC13; 
+#if defined(ARDUINO)
+#	ifdef ESP8266
+int led = 13;
+#	else
+int led = LED_BUILTIN;
+#	endif
 #endif
 
 // the setup routine runs once when you press reset:
@@ -17,9 +19,9 @@ void setup()
 	pinMode(led, OUTPUT);
 	Serial.begin(38400);
 	/*
-	while (!Serial) ; // wait for Arduino Serial Monitor
-	Serial.println("Ciao");
-	*/
+	   while (!Serial) ; // wait for Arduino Serial Monitor
+	   Serial.println("Ciao");
+	   */
 }
 
 void blink(int msec)
@@ -34,35 +36,35 @@ float f = 0;
 void loop()
 {
 	/*
-	auto p = new char [100];
-	delete []p;
-	*/
+	   auto p = new char [100];
+	   delete []p;
+	   */
 	blink(100);
 
 	/*
-	f += 0.1f;
-	if (f > 12)
-		f = 0;
+	   f += 0.1f;
+	   if (f > 12)
+	   f = 0;
 
-	Serial.println("Ciao");
-	*/
+	   Serial.println("Ciao");
+	   */
 	/*
-	f += 0.1;
-	if (f > 5)
-		Serial.println("f >");
-		*/
+	   f += 0.1;
+	   if (f > 5)
+	   Serial.println("f >");
+	   */
 }
 
 /*
 
-void loop() {
-	blink(100);
-	blink(100);
-	blink(1000);
-	blink(500);
-	f += 1.1 + sin(f);
+   void loop() {
+   blink(100);
+   blink(100);
+   blink(1000);
+   blink(500);
+   f += 1.1 + sin(f);
 
-	if (f > 30)
-	blink(1000);
-}
-*/
+   if (f > 30)
+   blink(1000);
+   }
+   */
